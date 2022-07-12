1938a63c8a359a02a7861cef6f0459d03f1d62fb
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-07-12 23:52:10 +0300

    finished assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -11,10 +12,14 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home key="5"name={user.name}
+      />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
+      
     </div>
   );
 }
+      
+   
 
 export default App;
